6b2358c61d558b652a6b79f5af9d0d9030a87cda
Shangzhen Yang <shangzheny01@gmail.com>
2022-05-20 15:10:55 -0700

    Add level feature

---
+++ src/Status.js
@@ -0,0 +1,22 @@
+function Status(props) {
+	let count = 0
+	for (const list of props.lists) {
+		count += list.words.length
+	}
+	let title
+	if (count < 10) {
+		title = "Little Monkey (Lv1)"
+	} else if (count < 20) {
+		title = "Monkey Master (Lv2)"
+	} else if (count < 30) {
+		title = "Monkey Monster (Lv3)"
+	} else {
+		title = "Monkey King (Lv4)"
+	}
+	return <>
+		<h2>Status</h2>
+		<div className="status">You have added {count} words.<br />You are now <strong>{title}</strong>!</div>
+	</>
+}
+
+export default Status
