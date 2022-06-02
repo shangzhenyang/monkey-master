3649fc7bb9600dd0b0f750a519bbc065993025f0
Shangzhen Yang <shangzheny01@gmail.com>
2022-06-02 16:56:19 -0700

    Fix quiz accessibility

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,16 +4,17 @@ export default function Quiz() {
 	return (
 		<main className="quiz">
 			<div className="container h-100">
-            	<div className="row h-100 justify-content-center align items-center">
-				<h1 >Quiz</h1>
-				<h2>Question 1/1</h2>
-				<label>
-    				<p>A Word</p>
-					<button className="quiz_button" title="refresh the page"><span className="icon">&#xe65e;</span></button>
-    				<input type="text" name="answer" />
-					<input type="submit" className= "submit" value="Check" />
-  				</label>
-			  </div>
+				<div className="row h-100 justify-content-center align items-center">
+					<h1>Quiz</h1>
+					<h2>Question 1/1</h2>
+					<div className="quiz-box">
+						<p>A Word</p>
+						<button className="quiz-button" title="refresh the page"><span className="icon">&#xe65e;</span></button>
+						<label htmlFor="answer">Enter the definition:</label>
+						<input id="answer" type="text" name="answer" />
+						<input type="submit" className="quiz-submit-button" value="Check" />
+					</div>
+				</div>
 			</div>
 		</main>
 	);
